74555fca906e68a5d49465ad8031d919a396b635
Roy <roeiklein@gmail.com>
2012-11-10 14:36:03 +0200

    Some more tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,6 +19,7 @@ describe User do
 		password: "foobar", password_confirmation: "foobar")}
 
 	subject {@user}
+
 	it {should respond_to(:name)}
 	it {should respond_to{:email}}
 	it {should respond_to(:password_digest)}
@@ -26,4 +27,98 @@ describe User do
 	it {should respond_to(:authenticate) }
 	it {should respond_to(:admin)}
 
+	it {should be_valid}
+	it {should_not be_admin}
+
+	describe "with admin attribute set to 'true'" do
+		before do
+			@user.save!
+			@user.toggle!(:admin)
+		end
+		it {should be_admin}
+	end
+
+	describe "remember token" do
+		before {@user.save}
+		its(:remember_token) {should_not be_blank}
+	end
+
+	describe "when name is not present" do
+		before {@user.name = " "}
+		it {should_not be_valid}
+	end
+
+	describe "when name is too long" do
+		before {@user.name = "a" * 51}
+		it {should_not be_valid}
+	end
+
+	describe "when mail is not present" do
+		before {@user.email = " "}
+		it {should_not be_valid}
+	end
+
+	describe "when password isn't present" do
+		before {@user.password = @user.password_confirmation = " "}
+		it {should_not be_valid}
+	end
+
+	describe "when password don't match" do
+		before {@user.password_confirmation = "mismatch"}
+		it {should_not be_valid}
+	end
+
+	describe "when password confirmation is nil" do
+	  before { @user.password_confirmation = nil }
+	  it { should_not be_valid }
+	end
+
+	describe "return value of authenticate method" do
+		before {@user.save}
+		let(:found_user) {User.find_by_email(@user.email)}
+
+		describe "with valid password" do
+			it {should == found_user.authenticate(@user.password)}
+		end
+		describe "with invalid password" do
+			let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+			it {should_not == user_for_invalid_password}
+			specify {user_for_invalid_password.should be_false}
+		end
+	end
+
+	describe "with a password that's too short" do
+	  before { @user.password = @user.password_confirmation = "a" * 5 }
+	  it { should be_invalid }
+	end
+
+	describe "when email format is invalid" do
+		it "should be invalid" do
+			addresses = %w[foo@bar foo@bar,com something.org lala.is@invalid. also@this@is.com way@bad+thing.com]
+			addresses.each do |addr|
+				@user.email = addr;
+				@user.should_not be_valid
+			end
+		end
+	end
+
+	describe "when email format is valid" do
+		it "should be valid" do
+			addresses = %w[user@foo.com a_us-er@f.o.org first.last@foo.jp a+b@baz.co.il]
+			addresses.each do |addr|
+				@user.email = addr;
+				@user.should be_valid
+			end
+		end
+	end
+
+	describe "when email is already taken" do
+		before do
+			user_same_mail = @user.dup
+			user_same_mail.email = @user.email.upcase
+			user_same_mail.save
+		end
+
+		it {should_not be_valid}
+	end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -9,13 +9,14 @@ describe "AuthenticationPages" do
 	    	before {click_button "Sign in"}
 	    	it { should have_selector('div.alert.alert-error', :text => 'Invalid') }
 		end
-		describe "with existing user" do
+		describe "with valid info" do
 			let(:user) { FactoryGirl.create(:user) }
 			before do
 				fill_in "Email",    with: user.email
 				fill_in "Password", with: user.password
 				click_button "Sign in"
 	      	end
+	      	it { should_not have_selector('div.alert.alert-error', :text => 'Invalid') }
 	      	it { should have_selector('title', text: user.name) }
 	    end
 	end
