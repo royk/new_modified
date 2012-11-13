a243f578007a1a7e30dedce9fcc42dfc46c7b933
Roy <roeiklein@gmail.com>
2012-11-13 06:31:20 +0200

    Feed appears to non-logged in users

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,7 @@
 class StaticPagesController < ApplicationController
 	require 'will_paginate/array' 
   def home
-  	if signed_in?
-  		@feed_items = current_user.feed.paginate(page: params[:page])
-  	else
-  		@feed_items = Post.all.paginate(page: params[:page])
-  	end
-
+  	current_user ||= User.new
+	@feed_items = current_user.feed.paginate(page: params[:page])
   end
 end
