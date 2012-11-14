f331c10405d9c476f3e67b05ce25843f493def58
Roy <roeiklein@gmail.com>
2012-11-14 19:50:49 +0200

    Added pagination (10 items per page) for home feed and videos feed.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,6 @@ class StaticPagesController < ApplicationController
 	require 'will_paginate/array' 
   def home
   	current_user ||= User.new
-	@feed_items = current_user.feed.paginate(page: params[:page])
+	@feed_items = current_user.feed.paginate(page: params[:page], :per_page=> 10)
   end
 end
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,7 +3,7 @@ class VideosController < ApplicationController
 	before_filter	:correct_user, only: :destroy
 
 	def index
-		@videos = Video.paginate(page: params[:page])
+		@videos = Video.paginate(page: params[:page], :per_page => 10)
 	end
 	
 	def create
