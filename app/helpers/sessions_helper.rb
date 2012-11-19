853eb791e2bb9c31a031021e5e24b3a1b4f3effc
Roy <roeiklein@gmail.com>
2012-11-19 09:16:29 +0200

    Kudos flyout. Redirect back disabled since it's very buggy.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -29,7 +29,8 @@ module SessionsHelper
 	end
 
 	def redirect_back_or(default)
-		redirect_to(session[:return_to] || default)
+		#redirect_to(session[:return_to] || default)
+		redirect_to(default)
 		session.delete(:return_to)
 	end
 
