ebb7429d8f61856e2064ac6ca2eac0c0565d0abd
royk <roeiklein@gmail.com>
2012-11-24 09:52:08 +0200

    Video editing implemented and tested

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,5 @@
 class VideosController < AuthenticatedController
-	before_filter	:correct_user, only: :destroy
+	before_filter	:correct_user, only: [:destroy, :update]
 
 	def show
     	@video = Video.find(params[:id])
@@ -12,17 +12,24 @@ class VideosController < AuthenticatedController
 	def create
 		@video = current_user.videos.build(params[:video])
 		@video.user_id = current_user.id
-		if parse_video
-			if @video.save
-				notify_activity_on(@video, current_user, nil)
-				flash[:success] = "Video created"
-			else
-				flash[:error] = "Video not saved: Already exists"
-			end
-		else
-			flash[:error] = "Unrecognized Video URL"
+		uid_vendor = get_uid_vendor(@video.url) if @video.url
+		if save_video(uid_vendor)
+			flash[:success] = "Video created"
+			notify_activity_on(@video, current_user, nil)
 		end
 		redirect_to root_url
+	end
+
+	def update
+		@video = Video.find(params[:id])
+		uid_vendor = get_uid_vendor(params[:video][:url])
+		@video.url = params[:video][:url]
+		if save_video(uid_vendor)
+			flash[:success] = "Video modified"
+			redirect_to @video
+		else
+			redirect_to root_url
+		end
 	end
 
 	def destroy
@@ -32,6 +39,22 @@ class VideosController < AuthenticatedController
 	end
 
 	private
+
+		def save_video(uid_vendor)
+			if uid_vendor
+				@video.vendor = uid_vendor[:vendor]
+				@video.uid = uid_vendor[:uid]
+				if @video.save
+					return true
+				else
+					flash[:error] = "Video not saved: Already exists"
+				end
+			else
+				flash[:error] = "Unrecognized Video URL"
+			end
+			return false
+		end
+
 		def correct_user
 			if !current_user.admin?
 				@video = current_user.videos.find_by_id(params[:id])
@@ -39,25 +62,19 @@ class VideosController < AuthenticatedController
 			end
 		end
 
-		def parse_video
-			vimeoID = @video.url.scan(/vimeo\.com\/(\d+)$/i)
-			youtubeIDLong = @video.url.scan(/(?:youtube\.com\/watch[^\s]*[\?&]v=)([\w-]+)/i);
-			youtubeIdShort = @video.url.scan(/(?:youtu\.be\/)([\w-]+)/i)
+		def get_uid_vendor(url)
+			vimeoID = url.scan(/vimeo\.com\/(\d+)$/i)
+			youtubeIDLong = url.scan(/(?:youtube\.com\/watch[^\s]*[\?&]v=)([\w-]+)/i);
+			youtubeIdShort = url.scan(/(?:youtu\.be\/)([\w-]+)/i)
 			ret = false
 			if vimeoID.any?
-				@video.uid = vimeoID[0][0]
-				@video.vendor = "vimeo"
-				ret = true
+				ret = {uid: vimeoID[0][0], vendor: "vimeo"}
 			end
 			if youtubeIDLong.any?
-				@video.uid = youtubeIDLong[0][0]
-				@video.vendor = "youtube"
-				ret = true
+				ret = {uid: youtubeIDLong[0][0], vendor: "youtube"}
 			end
 			if youtubeIdShort.any?
-				@video.uid = youtubeIdShort[0][0]
-				@video.vendor = "youtube"
-				ret = true
+				ret = {uid: youtubeIdShort[0][0], vendor: "youtube"}
 			end
 			return ret
 		end
--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe VideosController do 
 	let(:user) { FactoryGirl.create(:user) }
+	let(:user2) { FactoryGirl.create(:user) }
 	let(:video) { FactoryGirl.create(:video) }
 
 	before { sign_in user }
@@ -34,10 +35,11 @@ describe VideosController do
 		context "with long youtube url" do
 			it "should create a new youtube video" do
 				uid = "CoyEILQHJyc"
+				url = "http://www.youtube.com/watch?feature=player_embedded&v=#{uid}"
 				expect do
-					post :create, video: {uid: "http://www.youtube.com/watch?feature=player_embedded&v=#{uid}"}
+					post :create, video: {url: url}
 				end.to change(Video, :count).by(1)
-
+				assigns[:video][:url].should eq(url)
 				assigns[:video][:uid].should eq(uid)
 				assigns[:video][:vendor].should eq("youtube")
 			end
@@ -46,7 +48,7 @@ describe VideosController do
 			it "should create a new youtube video" do
 				uid = "CoyEILQHJyc"
 				expect do
-					post :create, video: {uid: "http://www.youtu.be/#{uid}"}
+					post :create, video: {url: "http://www.youtu.be/#{uid}"}
 				end.to change(Video, :count).by(1)
 
 				assigns[:video][:uid].should eq(uid)
@@ -57,7 +59,7 @@ describe VideosController do
 			it "should create a new vimeo video" do
 				uid = "53369314"
 				expect do
-					post :create, video: {uid: "http://vimeo.com/#{uid}"}
+					post :create, video: {url: "http://vimeo.com/#{uid}"}
 				end.to change(Video, :count).by(1)
 
 				assigns[:video][:uid].should eq(uid)
@@ -65,26 +67,87 @@ describe VideosController do
 				
 			end
 			it "should redirect to root" do
-				post :create, video: { uid: "http://vimeo.com/53369314" }
+				post :create, video: { url: "http://vimeo.com/53369314" }
 				response.should redirect_to root_url
 			end
 		end
 		context "with a non-video url" do
 			it "should not create a new video" do
 				expect do
-					post :create, video: { uid: "https://www.google.com/search?q=rspec+running+specific&oq=rspec+running+specific&aqs=chrome.0.57j60.6091&sugexp=chrome,mod=3&sourceid=chrome&ie=UTF-8#hl=en&tbo=d&biw=1163&bih=817&sclient=psy-ab&q=rspec+create+get+created+&oq=rspec+create+get+created+&gs_l=serp.3...3500.4636.7.4981.12.11.0.0.0.0.221.1825.0j10j1.11.0.les%3B..0.0...1c.1.RkNzV62l5c4&pbx=1&bav=on.2,or.r_gc.r_pw.r_cp.r_qf.&fp=6be1df0b8d8277dc&bpcl=38626820" }
+					post :create, video: { url: "https://www.google.com/search?q=rspec+running+specific&oq=rspec+running+specific&aqs=chrome.0.57j60.6091&sugexp=chrome,mod=3&sourceid=chrome&ie=UTF-8#hl=en&tbo=d&biw=1163&bih=817&sclient=psy-ab&q=rspec+create+get+created+&oq=rspec+create+get+created+&gs_l=serp.3...3500.4636.7.4981.12.11.0.0.0.0.221.1825.0j10j1.11.0.les%3B..0.0...1c.1.RkNzV62l5c4&pbx=1&bav=on.2,or.r_gc.r_pw.r_cp.r_qf.&fp=6be1df0b8d8277dc&bpcl=38626820" }
 				end.not_to change(Video, :count)
 			end
 			it "should redirect to root" do
-				post :create, video: { uid: "https://www.google.com/search?q=rspec+running+specific&oq=rspec+running+specific&aqs=chrome.0.57j60.6091&sugexp=chrome,mod=3&sourceid=chrome&ie=UTF-8#hl=en&tbo=d&biw=1163&bih=817&sclient=psy-ab&q=rspec+create+get+created+&oq=rspec+create+get+created+&gs_l=serp.3...3500.4636.7.4981.12.11.0.0.0.0.221.1825.0j10j1.11.0.les%3B..0.0...1c.1.RkNzV62l5c4&pbx=1&bav=on.2,or.r_gc.r_pw.r_cp.r_qf.&fp=6be1df0b8d8277dc&bpcl=38626820" }
+				post :create, video: { url: "https://www.google.com/search?q=rspec+running+specific&oq=rspec+running+specific&aqs=chrome.0.57j60.6091&sugexp=chrome,mod=3&sourceid=chrome&ie=UTF-8#hl=en&tbo=d&biw=1163&bih=817&sclient=psy-ab&q=rspec+create+get+created+&oq=rspec+create+get+created+&gs_l=serp.3...3500.4636.7.4981.12.11.0.0.0.0.221.1825.0j10j1.11.0.les%3B..0.0...1c.1.RkNzV62l5c4&pbx=1&bav=on.2,or.r_gc.r_pw.r_cp.r_qf.&fp=6be1df0b8d8277dc&bpcl=38626820" }
 				response.should redirect_to root_url
 			end
 		end
 	end
 
+	describe "Update" do
+		before do
+			@to_edit = Video.create
+			@to_edit.url = "http://vimeo.com/53369314"
+			@to_edit.uid = "53369314"
+			@to_edit.vendor = "vimeo"
+		end
+		context "with wrong user" do
+			before do
+				@to_edit.user = user2
+				@to_edit.save!
+			end
+			it "should not change the video's url" do
+				put :update, id: @to_edit, video: {url: "http://www.youtu.be/CoyEILQHJyc"}
+
+				assigns[:video].should be_nil
+				@to_edit.reload
+				@to_edit.url.should_not eq("http://www.youtu.be/CoyEILQHJyc")
+			end
+		end
+
+		context "with new url" do
+			before do
+				@to_edit.user = user
+				@to_edit.save!
+			end
+			it "should change the video's url" do
+				put :update, id: @to_edit, video: {url: "http://www.youtu.be/CoyEILQHJyc"}
+
+				response.should redirect_to @to_edit
+				assigns[:video].should eq(@to_edit)
+				assigns[:video][:uid].should eq("CoyEILQHJyc")
+				@to_edit.reload
+				@to_edit.url.should eq("http://www.youtu.be/CoyEILQHJyc")
+			end
+		end
+
+		context "with already existing uid" do
+			before do
+				@other = Video.create
+				@other.url = "http://www.youtu.be/CoyEILQHJyc"
+				@other.uid = "CoyEILQHJyc"
+				@other.vendor = "vimeo"
+				@other.user = user
+				@other.save!
+
+				@to_edit.user = user
+				@to_edit.save!
+			end
+			it "should not change the video's url" do
+				put :update, id: @to_edit, video: {url: "http://www.youtu.be/CoyEILQHJyc"}
+
+				response.should_not redirect_to @to_edit
+				@to_edit.reload
+				@to_edit.url.should_not eq("http://www.youtu.be/CoyEILQHJyc")
+
+			end
+		end
+
+	end
+
 	describe "Destroy" do
 		before :each do
-			post :create, video: { uid: "http://vimeo.com/53369314" }
+			post :create, video: { url: "http://vimeo.com/53369314" }
 		end
 		it "should delete video" do
 			expect do
