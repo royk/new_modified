93e3b4ffdc1868dc0e7eb3a814c10107c4b1dfa8
royk <roeiklein@gmail.com>
2012-11-26 07:12:46 +0200

    Fixed migration

--- db/migrate/20121111200400_create_posts.rb
+++ db/migrate/20121111200400_create_posts.rb
@@ -3,8 +3,18 @@ class CreatePosts < ActiveRecord::Migration
     create_table :posts do |t|
       t.text :content
       t.integer :user_id
-
       t.timestamps
-    end
+  end
+      
+	 create_table :users do |t|
+	 	t.string :name
+	 	t.string :email
+	 	t.string :remember_token
+	 	t.string :password_digest
+	 	t.boolean :admin
+	 	t.timestamps
+	 end
+    add_index	:users, :email, unique: true
+    add_index	:users, :remember_token
   end
 end
