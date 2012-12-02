7eca050d0f1d7df2bd87873c44fcda942c12b0c9
royk <roeiklein@gmail.com>
2012-12-02 21:57:16 +0200

    Fixed footer kudos (since item id moved to feed counter, i couldn't know the correct id when re-rendering the item, now I pass it with the request so I know in the response where to put it)

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,7 @@ class LikesController < ResponseController
 
 	def create
 		if super
-			render partial: 'shared/feed_item', locals: {feed_item: find_parent}
+			render partial: 'shared/feed_item_internal', locals: {feed_item: find_parent, feed_item_counter: params[:index]}
 		else
 			logger.error "ERROR: LikesController.create: #{params.inspect}"
 			redirect_to request.referer
@@ -26,7 +26,7 @@ class LikesController < ResponseController
 		if parent
 			res = parent.likes.where("liker_id = "+current_user.id.to_s)
 			res.first.destroy if res.any? && res.first.liker.id==current_user.id
-			render partial: 'shared/feed_item', locals: {feed_item: parent}
+			render partial: 'shared/feed_item_internal', locals: {feed_item: parent, feed_item_counter: params[:index]}
 		else
 			logger.error "ERROR: LikesController.delete: #{params.inspect}"
 			render :status => 500
