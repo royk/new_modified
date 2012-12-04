fc9d7fef565f9b7bcf856ca6cc4c157219cb766d
royk <roeiklein@gmail.com>
2012-12-04 22:50:39 +0200

    Main feed sorts by update date, not create date.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
 	require 'will_paginate/array' 
   def home
   	per_page = params[:items_count] || 10
-	@feed_items = Post.where(sticky: true).order("created_at DESC") + ((privacy_query(Post.where(sticky: false)) + privacy_query(Video)).sort_by {|f| -f.created_at.to_i}) # sort by descending by converting to int and negating
+	@feed_items = Post.where(sticky: true).order("updated_at DESC") + ((privacy_query(Post.where(sticky: false)) + privacy_query(Video)).sort_by {|f| -f.updated_at.to_i}) # sort by descending by converting to int and negating
 	@feed_items = @feed_items.paginate(page: params[:page], :per_page=> per_page)	
 	if request.xhr?
 		render partial: 'shared/feed_item', collection: @feed_items, comments_shown: false
