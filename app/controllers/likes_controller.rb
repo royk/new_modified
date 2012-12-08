e7c9cc9041621959ead2ef39afdbbbd7b30c602b
royk <roeiklein@gmail.com>
2012-12-08 20:28:07 +0200

    Removed redundant check

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -25,10 +25,9 @@ class LikesController < ResponseController
 		parent = find_parent
 		if parent
 			res = parent.likes.where("liker_id = "+current_user.id.to_s)
-			res.first.destroy if res.any? && res.first.liker.id==current_user.id
+			res.first.destroy if res.any?
 			render partial: 'shared/feed_item_internal', locals: {feed_item: parent, feed_item_counter: params[:index]}
 		else
-			logger.error "ERROR: LikesController.delete: #{params.inspect}"
 			render :status => 500
 		end
 	end
