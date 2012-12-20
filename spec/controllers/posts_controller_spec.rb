4cb96c1f86732355b461ea07917b9a18f3039c7f
Roy klein <roy.k@wix.com>
2012-12-20 13:30:56 +0200

    Added tests to posts_controller#create

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe PostsController do 
 	let(:user) { FactoryGirl.create(:user) }
-	let(:post) { FactoryGirl.create(:post, user: user) }
+	let(:post_item) { FactoryGirl.create(:post, user: user) }
 
 	before do
 		request.env["HTTP_REFERER"] = "where_i_came_from"
@@ -10,9 +10,34 @@ describe PostsController do
 
 	shared_examples_for "a successful get" do
 		it "should get the correct post" do
-			get :show, id: post
-			assigns(:post).should eq(post)
+			get :show, id: post_item
+			assigns(:post).should eq(post_item)
 		end
+	end
+
+	describe "create->" do
+		before { @content = "new post content" }
+		context "when signed out" do
+			it "should not create" do
+				expect do
+					post :create, post: {content: @content}
+				end.to_not change(Post, :count)
+			end
+		end
+		context "when signed in" do
+			before { sign_in user }
+			it "should create" do
+				expect do
+					post :create, post: {content: @content}
+				end.to change(Post, :count).by 1
+			end
+			it "should create a notification" do
+				expect do
+					post :create, post: {content: @content}
+				end.to change(Notification, :count).by 1
+			end
+		end
+
 	end
 
 	describe "show->" do
@@ -22,11 +47,11 @@ describe PostsController do
 			end
 			context "and getting private post" do
 				before do
-					post.public = false
-					post.save!
+					post_item.public = false
+					post_item.save!
 				end
 				it "should not return the post" do
-					get :show, id: post
+					get :show, id: post_item
 					assigns(:post).should be_nil
 					response.should_not render_template :show
 				end
@@ -41,8 +66,8 @@ describe PostsController do
 			end
 			context "and getting private post" do
 				before do
-					post.public = false
-					post.save!
+					post_item.public = false
+					post_item.save!
 				end
 				it_behaves_like "a successful get"
 			end
@@ -51,7 +76,7 @@ describe PostsController do
 	describe "edit->" do
 		context "when logged out" do
 			it "should not allow editing" do
-				get :edit, id:post
+				get :edit, id:post_item
 				response.should_not render_template :edit
 			end
 		end
@@ -61,7 +86,7 @@ describe PostsController do
 					sign_in user
 				end
 				it "should allow editing" do
-					get :edit, id:post
+					get :edit, id:post_item
 					response.should render_template :edit
 				end
 			end
@@ -77,7 +102,7 @@ describe PostsController do
 					sign_in @non_owner
 				end
 				it "should not allow editing" do
-					get :edit, id:post
+					get :edit, id:post_item
 					response.should_not render_template :edit
 				end
 			end
@@ -93,7 +118,7 @@ describe PostsController do
 					sign_in @non_owner
 				end
 				it "should allow editing" do
-					get :edit, id:post
+					get :edit, id:post_item
 					response.should render_template :edit
 				end
 			end
