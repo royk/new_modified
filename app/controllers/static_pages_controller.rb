23ccb669b63644e55c54b4e674d35f9d9dc6896a
Roy <roeiklein@gmail.com>
2012-12-25 22:04:22 +0200

    First time user message.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
 	include ApplicationHelper
 	require 'will_paginate/array' 
 	def home
-	  	@first_time = first_time_visitor?
+	  	@first_timer = first_time_visitor?
 	  	per_page = params[:items_count] || 10
 		@feed_items = Post.where(sticky: true).order("updated_at DESC") + ((privacy_query(Post.where(sticky: false)) + privacy_query(Video)).sort_by {|f| -f.updated_at.to_i}) # sort by descending by converting to int and negating
 		@feed_items = @feed_items.paginate(page: params[:page], :per_page=> per_page)	
