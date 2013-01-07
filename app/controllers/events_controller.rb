43f924df9b584292e303ce4dd2ea2a39362ad615
Roy <roeiklein@gmail.com>
2013-01-07 19:46:43 +0200

    Sorted events display.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,17 @@
 class EventsController < AuthenticatedController
 	skip_before_filter :signed_in_user, only: [:index, :show]
 
+	def index
+		@past_events = Event.where("start_date < ?", Time.now).order('start_date desc')
+		@upcoming_events = Event.where("start_date >= ?", Time.now).order('start_date desc')
+	end
+
 	def create
 		create! { request.referer }
 	end
 
 	def update
-		@event = Event.find(params[:id])
+		
 		unless @event.nil?
 			if params[:attendant]
 				@user = User.find(params[:attendant])
