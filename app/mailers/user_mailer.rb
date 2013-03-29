64b5cf347a44c782764cf6f388b9d1ef26ee85da
Roy <roeiklein@gmail.com>
2013-03-29 09:58:02 +0300

    Fixed reset password

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -12,7 +12,7 @@ class UserMailer < ActionMailer::Base
 
   def forgot_pwd_mail(user)
   	@user = user
-  	@url = "#{site_url}reset#{@user.create_reset_code}"
+  	@url = "#{site_url}reset/#{@user.create_reset_code}"
     @site_name = site_name
   	mail(to: user.email, subject: "Reset password for #{site_name}")
   end
