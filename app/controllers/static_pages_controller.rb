7ac9ff77985196f2cf72c2254ca284173776a5a7
Roy <roeiklein@gmail.com>
2013-03-31 21:30:41 +0300

    Front page for non-registered users uses front-page article.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,18 +5,25 @@ class StaticPagesController < ApplicationController
 
 	def home
 	  	@first_timer = first_time_visitor?
-	  	per_page = params[:items_count] || 10
-	  	@feed = Feed.find_by_store_name("main feed")
-  		@feed = seed_main_feed if @feed.nil?
-		@feed_items = @feed.feed_items(BlogPost) do |collection|
-			privacy_query(collection)
-		end
-		@feed_items = @feed_items.paginate(page: params[:page], :per_page=> per_page)
-		if mobile_device?
-			@feeds = Feed.where("hidden = ?", false)
-		end
-		if request.xhr?
-			render partial: 'shared/feed_item', collection: @feed_items, comments_shown: false
+	  	if mobile_device? || signed_in? || request.xhr?
+	  		@feed_front_page = true
+		  	per_page = params[:items_count] || 10
+		  	@feed = Feed.find_by_store_name("main feed")
+	  		@feed = seed_main_feed if @feed.nil?
+			@feed_items = @feed.feed_items(BlogPost) do |collection|
+				privacy_query(collection)
+			end
+			@feed_items = @feed_items.paginate(page: params[:page], :per_page=> per_page)
+			if mobile_device?
+				@feeds = Feed.where("hidden = ?", false)
+			end
+			if request.xhr?
+				render partial: 'shared/feed_item', collection: @feed_items, comments_shown: false
+			end
+		else
+			@full_site_layout = true	
+			@feed_front_page = false
+			@article = Article.find_by_permalink("front-page")
 		end
 	end
 
