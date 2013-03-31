a545d66e4a7283b72a27bfe5bed2c2e40583905e
Roy <roeiklein@gmail.com>
2013-03-31 23:55:07 +0300

    Default gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,12 @@ module UsersHelper
 		unless user.nil?
 		    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
 		    size = options[:size]
-		    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+		    gravatar_url = "http://en.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=404"
+		    url = URI.parse(gravatar_url)
+		    res = Net::HTTP.get(url)
+		    if res=="404 Not Found"
+		    	gravatar_url = "https://secure.gravatar.com/avatar/2cc2c37963368cc3c03580a665aae8a7?s=#{40}"
+		    end
 		    return image_tag(gravatar_url, alt: user.shown_name, class: "gravatar", width: size, height: size)
 		else
 			gravatar_url = "https://secure.gravatar.com/avatar/2cc2c37963368cc3c03580a665aae8a7?s=#{40}"
