5761e387afe5ae6382848d528e3743dacdf48ef9
Roy <roeiklein@gmail.com>
2013-05-19 08:08:44 +0300

    Search video conforms to new style

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -23,6 +23,9 @@ class VideosController < AuthenticatedController
 
 	def feedback_index
 		@videos = Video.where("#{compound_privacy_query} for_feedback = ? ", true).paginate(page: params[:page], :per_page => 10)
+		if request.xhr?
+			render partial: 'shared/videos/small/small_video_item', collection: @videos, comments_shown: false
+		end
 	end
 	
 	def create
@@ -62,7 +65,7 @@ class VideosController < AuthenticatedController
 	end
 
 	def search
-		@videos = Video.tagged_with(params[:search])
+		@videos = Video.tagged_with(params[:search]).paginate(page: params[:page], :per_page => 20)
 	end
 
 	private
