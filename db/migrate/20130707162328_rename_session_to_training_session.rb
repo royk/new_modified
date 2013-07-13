100bbfce0f23dc61c63e7f8d153e9b5d3e2c5147
Roy <roeiklein@gmail.com>
2013-07-13 21:20:11 +0300

    Removed bad migration file

--- db/migrate/20130707162328_rename_session_to_training_session.rb
+++
@@ -1,9 +0,0 @@
-class RenameSessionToTrainingSession < ActiveRecord::Migration
-	def up
-		rename_table :sessions, :training_sessions
-	end
-
-	def down
-		rename_table :training_sessions, :sessions
-	end
-end
