cbe9f33aac4e28a2bf755a70781196c2b79badb6
Roy <roeiklein@gmail.com>
2013-08-11 08:01:01 +0300

    Fix for error 500 mailer exception.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,12 @@ class ApplicationController < ActionController::Base
 	rescue_from Exception, :with => :handle_exceptions
 	private
 	def handle_exceptions(e)
-		ErrorMailer.experror(e).deliver
-		render :template => "error_mailer/500", :status => 500
+		if Rails.env.production?
+			ErrorMailer.experror(e).deliver
+			render :template => "error_mailer/500", :status => 500
+		else
+			raise e
+		end
 	end
   protect_from_forgery
   include SessionsHelper
