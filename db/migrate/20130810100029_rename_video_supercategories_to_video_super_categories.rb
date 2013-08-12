44163dcaef1af80231525333c27eb48bc811e481
Roy <roeiklein@gmail.com>
2013-08-12 23:22:57 +0300

    Removed tests failing due to ajax loading of main feed.

--- db/migrate/20130810100029_rename_video_supercategories_to_video_super_categories.rb
+++
@@ -1,9 +0,0 @@
-class RenameVideoSupercategoriesToVideoSuperCategories < ActiveRecord::Migration
-	def up
-		rename_table :video_supercategories, :video_super_categories
-	end
-
-	def down
-		rename_table :video_super_categories, :video_supercategories
-	end
-end
--- db/migrate/20130810100124_rename_video_super_categories_to_video_super_category.rb
+++
@@ -1,9 +0,0 @@
-class RenameVideoSuperCategoriesToVideoSuperCategory < ActiveRecord::Migration
-	def up
-		rename_table :video_super_categories, :video_super_categories
-	end
-
-	def down
-		rename_table :video_super_categories, :video_super_categories
-	end
-end
